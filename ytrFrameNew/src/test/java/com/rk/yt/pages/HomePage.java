773cbefda300b0b884e075fce2679ef55c173fd9
Rahul <thisisdemmo@gmail.com>
2019-12-11 01:02:15 +0530

    conflict code

--- ytrFrameNew/src/test/java/com/rk/yt/pages/HomePage.java
+++ ytrFrameNew/src/test/java/com/rk/yt/pages/HomePage.java
@@ -4,11 +4,11 @@ import org.openqa.selenium.By;
 
 public class HomePage extends TestBase{
 	
-//	public String profileDet() {
+	public void profileDem() {
 //		driver.findElement(By.xpath(prop.getProperty("avatar"))).click();
 //		String text=driver.findElement(By.xpath(prop.getProperty("profileemail"))).getText();
-//		return text;
+//		return String;
 //		
-//	}
+}
 
 }
